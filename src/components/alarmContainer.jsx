6c7435f78bd76b33329f31631726d9fcc489b24b
Soumyaranjann <123076223+Soumyaranjann@users.noreply.github.com>
2023-03-17 12:28:05 +0530

    Update alarmContainer.jsx

--- src/components/alarmContainer.jsx
+++ src/components/alarmContainer.jsx
@@ -1,39 +1,34 @@
 import React from "react";
-import { Add } from "@mui/icons-material";
-import { Fab } from "@mui/material";
 import "../styles/App.css";
-import Alarm from "./alarm";
+const App = () => {
+  let numClicks = 0;
+  let singleClickTimer;
+  const handleClick = () => {
+    numClicks++;
+    if (numClicks === 1) {
+      singleClickTimer = setTimeout(() => {
+        numClicks = 0;
+        console.log("I was not double clicked");
+      }, 400);
+    } else if (numClicks === 2) {
+      clearTimeout(singleClickTimer);
+      numClicks = 0;
+      console.log("I was double clicked");
+    }
+  };
 
-export default function AlarmContainer(props) {
-  const alarms = [
-    {
-        label: "Interview at abc", 
-        alarmTime: new Date(Date.now() + 30*60*1000)
-    },
-    {
-        label: "Interview at xyz", 
-        alarmTime: new Date(Date.now() + 100*60*1000)
-    },
-    {
-        label: "Interview at pqr", 
-        alarmTime: new Date(Date.now() + 200*60*1000)
-    },
-]
+  return (
+    <div id="main">
+      <button
+        id="dblclick-btn"
+        onClick={(event) => {
+          handleClick(event);
+        }}
+      >
+        Double click me
+      </button>
+    </div>
+  );
+};
 
-const handleEdit = (label, alarmTime, index) => {
-    // console.log("hello")
-    props.onEdit(label, alarmTime, index)
-}
-    return (
-        <>
-        <Fab color="primary" aria-label="add" onClick={props.onOpen} id="add-new">
-            <Add />
-          </Fab>
-          <div id="alarms">
-          {props.alarms.length === 0 ? "No alarms found" : props.alarms.map((alarm, index) => (
-            <Alarm key={alarm.label} label={alarm.label} alarmTime={alarm.alarmTime} onDelete={()=> props.onDelete(index)} time={props.time} onEdit={(label1, alarmTime1)=>handleEdit(label1, alarmTime1, index)} index={index} onSnooze={() => props.onSnooze(index)}/>
-          ))}
-          </div>
-        </>
-    )
-}
+export default App;
